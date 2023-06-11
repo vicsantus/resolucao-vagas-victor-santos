7671ff88909c756f5ee0c4183ed3ee52a0ee14fd
victrosantos <victor.santos.fk@hotmail.com>
2023-06-11 18:55:31 -0300

    feat: Melhorado performance do arquivo teste3.js

--- teste3.js
+++ teste3.js
@@ -1,15 +1,26 @@
-var data =  require("./fakeData");
+const data = require('./fakeData');
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+module.exports = function (req, res) {
+  const { name } = req.query;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+  // Para deixar o código antigo mais performatico bastava adicionar um break no laço for, porém, eu prefiro fazer usando forEach,
+  // pois fica muito mais facil de ler o código
+
+  // Esse código poderia ser utiliza também algum algoritmo de busca, porém, o requisito pede para deixar mais performatico, e um algoritmo
+  // de busca mais avançado deixaria um teste com um db curto mais complexo e não menos.
+
+  let found = false;
+
+  data.forEach((obj) => {
+    if (obj.name === name) {
+      obj = null;
+      found = true;
+      return;
     }
+  });
 
-    res.send("success");
-
+  if (found) {
+    return res.send('success');
+  }
+  return res.send('Object not found');
 };
